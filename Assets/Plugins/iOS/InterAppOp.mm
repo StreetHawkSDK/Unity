2ec9fdeb41d90feb4145d3a5bd9d1e8af323319c
appmechanic <team.mechanic2014@gmail.com>
2015-04-14 23:19:07 +0530

    Unity SDK
    Unity SDK

---
+++ Assets/Plugins/iOS/InterAppOp.mm
@@ -0,0 +1,53 @@
+#import <UIKit/UIKit.h>
+#import "UnityAppController.h"
+ 
+ @interface InterAppOp: UnityAppController
+ {
+ }
+ 
+ 
+ -(BOOL) application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation;
+ 
+ @end
+ @implementation InterAppOp 
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
+{
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    if ([launchOptions objectForKey:UIApplicationLaunchOptionsURLKey]) {
+        NSURL *url = [launchOptions objectForKey:UIApplicationLaunchOptionsURLKey];
+        
+        [self performSelector:@selector(openURLAfterDelay:) withObject:url afterDelay:2];
+    }
+    
+    return YES;
+}
+
+- (void) openURLAfterDelay:(NSURL*) url
+{
+    
+    UnitySendMessage("Deeplink", "URLHandler", [[url absoluteString] UTF8String]);
+}
+
+-(BOOL) application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    
+    UnitySendMessage("Deeplink", "URLHandler", [[url absoluteString] UTF8String]);
+    
+    return YES;
+    
+}
+
+ -(BOOL) application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+ 
+ {
+     UnitySendMessage("Deeplink", "URLHandler", [[url absoluteString] UTF8String]);
+     return YES;
+     
+ }
+ @end
+ 
+ 
+ 
+ IMPL_APP_CONTROLLER_SUBCLASS(InterAppOp)
