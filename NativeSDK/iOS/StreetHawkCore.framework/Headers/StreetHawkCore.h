590519c9ae33e5ed830474db95696f79a577c002
ChristineXYS <flychristine@gmail.com>
2015-05-07 14:38:42 +0800

    Jasmeet commit remove iOS native SDK, add it back.

---
+++ NativeSDK/iOS/StreetHawkCore.framework/Headers/StreetHawkCore.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2012 StreetHawk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by Christine XYS. 
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreetHawkCore.
+FOUNDATION_EXPORT double StreetHawkCoreVersionNumber;
+
+//! Project version string for StreetHawkCore.
+FOUNDATION_EXPORT const unsigned char StreetHawkCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreetHawkCore/PublicHeader.h>
+
+//folder Public/
+#import <StreetHawkCore/ISHCustomiseHandler.h>
+#import <StreetHawkCore/ISHPhonegapObserver.h>
+#import <StreetHawkCore/StreetHawkApp.h>
+#import <StreetHawkCore/StreetHawkLocationManager.h>
+#import <StreetHawkCore/StreetHawkNotificationHandler.h>
+#import <StreetHawkCore/StreetHawkRequest.h>
+#import <StreetHawkCore/StreetHawkUtils.h>
+
+//folder Public/Internal/
+#import <StreetHawkCore/StreetHawkCrashHandler.h>
+#import <StreetHawkCore/StreetHawkInstallHandler.h>
+#import <StreetHawkCore/StreetHawkLogger.h>
+#import <StreetHawkCore/StreetHawkRateHandler.h>
+
+//folder Public/Object/
+#import <StreetHawkCore/StreetHawkFeedObject.h>
+#import <StreetHawkCore/StreetHawkFriendlyNameObject.h>
+#import <StreetHawkCore/StreetHawkInstall.h>
+#import <StreetHawkCore/StreetHawkObject.h>
+
+//folder Public/UI/
+#import <StreetHawkCore/StreetHawkActionSheet.h>
+#import <StreetHawkCore/StreetHawkAlertSettingsViewController.h>
+#import <StreetHawkCore/StreetHawkAlertView.h>
+#import <StreetHawkCore/StreetHawkBaseViewController.h>
+#import <StreetHawkCore/StreetHawkSlideViewController.h>
+
